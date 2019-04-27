5af1f22f98cab7d34442b6c78a88bded3cffd1e5
ubakry <ubakry94@gmail.com>
2019-04-27 22:27:49 +0200

    picard

--- ngs2-assign.sh
+++ ngs2-assign.sh
@@ -1,18 +1,93 @@
-# Exploring the sample names
+###########################################
+#  - NGS 2 Course - Assignment            #
+#  - Bash Script                          #
+#  - April 26,2019                        #
+#  - Copyright: Usama Bakry               #
+#  - Nile University                      #
+###########################################
+#!/bin/bash
+
+# Downloading reference from gencode website
+echo "[   PROCESS   ]     Downloading reference from gencode website..."
+mkdir -p ../ref/
+wget -P ../ref/ http://genomedata.org/rnaseq-tutorial/fasta/GRCh38/chr22_with_ERCC92.fa
+echo "[     OK      ]      Reference is downloaded."
+
+# Exploring the samples names
 ls -tral ../data
 
+# Renaming the samples names
+mv ../data/SRR8797509_1.part_001.part_001.fastq.gz ../data/S1_L001_R1_001.fastq.gz
+mv ../data/SRR8797509_2.part_001.part_001.fastq.gz ../data/S1_L001_R2_001.fastq.gz
+
+mv ../data/shuffled_SRR8797509_1.part_001.part_001.fastq.gz ../data/S2_L001_R1_001.fastq.gz
+mv ../data/shuffled_SRR8797509_2.part_001.part_001.fastq.gz ../data/S2_L001_R2_001.fastq.gz
+
+# Indexing the reference genome
+echo "[   PROCESS   ]     Indexing the reference genome..."
+GENOME_DIR="../ref/star-index/"
+mkdir -p $GENOME_DIR && cd $GENOME_DIR
+ln -s ../chr22_with_ERCC92.fa .
+cd -
+STAR --runMode genomeGenerate --genomeDir $GENOME_DIR --genomeFastaFiles ../ref/chr22_with_ERCC92.fa  --runThreadN 4
+echo "[     OK      ] Indexing is done" 
+
+# Unzipping samples fastq files
+cd ../data/
+gunzip -k *.fastq.gz
+cd -
+
 # Add [Read group information] and align all reads
-mkdir -p ../GATK_results
-for R1 in ~/workdir/fqData/*_R1_001.pe.fq.gz;do
-    SM=$(basename $R1 | cut -d"_" -f1)                                          ##sample ID
-    LB=$(basename $R1 | cut -d"_" -f1,2)                                        ##library ID
+PASS_1_DIR=../GATK_results/star_res/1pass
+mkdir -p $PASS_1_DIR
+for R1 in ../data/*_R1_001.fastq;do
+    mkdir -p $PASS_1_DIR/$(basename $R1 _R1_001.fastq)
+    cd $PASS_1_DIR/$(basename $R1 _R1_001.fastq)
+    R2=$(echo $R1 | sed 's/_R1_/_R2_/')
+    echo $R1 $R2
+    STAR --genomeDir ../../../../ref/star-index/ --readFilesIn ../../../$R1 ../../../$R2 --runThreadN 4
+    cd -
+done
+
+# Indexing the reference genome
+echo "[   PROCESS   ]     Indexing the reference genome..."
+for S in {1..2};do
+    GENOME_DIR="../ref/star-index-2/S"$S
+    mkdir -p $GENOME_DIR && cd $GENOME_DIR
+    ln -s ../../chr22_with_ERCC92.fa .
+    cd -
+    STAR --runMode genomeGenerate --genomeDir $GENOME_DIR --genomeFastaFiles ../ref/chr22_with_ERCC92.fa --sjdbFileChrStartEnd ../GATK_results/star_res/1pass/S${S}_L001/SJ.out.tab --sjdbOverhang 75 --runThreadN 4
+    echo "[     OK      ] Indexing is done for sample $S" 
+done
+
+# Add [Read group information] and align all reads
+PASS_2_DIR=../GATK_results/star_res/2pass
+mkdir -p $PASS_2_DIR
+for R1 in ../data/*_R1_001.fastq;do
+    mkdir -p $PASS_2_DIR/$(basename $R1 _R1_001.fastq)
+    cd $PASS_2_DIR/$(basename $R1 _R1_001.fastq)
+    R2=$(echo $R1 | sed 's/_R1_/_R2_/')
+    echo $R1 $R2
+    echo ../../../../ref/star-index-2/$(basename $R1 | cut -d"_" -f1)
+    STAR --genomeDir ../../../../ref/star-index-2/$(basename $R1 | cut -d"_" -f1) --readFilesIn ../../../$R1 ../../../$R2 --runThreadN 4
+    cd -
+done
+
+
+# Add [Read group information]
+mkdir -p ../GATK_results/picard_res
+for i in S1_L001;do
+    SM=$(basename $i | cut -d"_" -f1)                                          ##sample ID
+    LB=$i                                        ##library ID
     PL="Illumina"                                                           ##platform (e.g. illumina, solid)
-    RGID=$(zcat $R1 | head -n1 | sed 's/:/_/g' |cut -d "_" -f1,2,3,4)       ##read group identifier 
+    RGID=$(cat ../data/${i}_R1_001.fastq | head -n1 | sed 's/ /_/g' | cut -d "_" -f1)       ##read group identifier 
     PU=$RGID.$LB                                                            ##Platform Unit
     echo -e "@RG\tID:$RGID\tSM:$SM\tPL:$PL\tLB:$LB\tPU:$PU"
 
-    R2=$(echo $R1 | sed 's/_R1_/_R2_/')
-    echo $R1 $R2
-    index="$HOME/workdir/bwa_align/bwaIndex/dog_chr5.fa"
-    bwa mem -t 4 -M -R "@RG\tID:$RGID\tSM:$SM\tPL:$PL\tLB:$LB\tPU:$PU" $index $R1 $R2 > $(basename $R1 _R1_001.pe.fq.gz).sam
+    picard AddOrReplaceReadGroups I=../GATK_results/star_res/2pass/$i/Aligned.out.sam O=../GATK_results/picard_res/${SM}_rg_added_sorted.bam SO=coordinate RGID=$RGID RGLB=$LB RGPL=$PL RGPU=$PU RGSM=$SM 
+
+    picard MarkDuplicates I=../GATK_results/picard_res/${SM}_rg_added_sorted.bam O=../GATK_results/picard_res/${SM}_dedupped.bam  CREATE_INDEX=true VALIDATION_STRINGENCY=SILENT M=../GATK_results/picard_res/${SM}_output.metrics 
 done
+
+
+gatk -T SplitNCigarReads -R ref.fasta -I dedupped.bam -o split.bam -rf ReassignOneMappingQuality -RMQF 255 -RMQT 60 -U ALLOW_N_CIGAR_READS
